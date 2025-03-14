2fc82c786318c78592a4a9ce6b0f71df9f22d47c
Valentino-R <valentino.orif@gmail.com>
2025-03-14 18:58:33 +0100

    pvp on

--- Exercices/ticTacToePvp.cpp
+++ Exercices/ticTacToePvp.cpp
@@ -19,7 +19,7 @@ int main ()
 
     while (running)
     {
-        palyerMove(spaces, player1);
+        palyer1Move(spaces, player1);
         drawBoard(spaces);
         if (checkWin(spaces, player1, player2))
         {
@@ -72,7 +72,7 @@ void palyer1Move(char* spaces, char player1)
     int number{};
     do
     {
-        std::cout << "Enter a spot to place a marker (1-9) : ";
+        std::cout << "Player 1 enter a spot to place an X (1-9) : ";
         std::cin >> number;
         number--;
         if (spaces[number] == ' ')
@@ -89,7 +89,7 @@ void player2Move(char* spaces, char player2)
     int number{};
     do
     {
-        std::cout << "Enter a spot to place a marker (1-9) : ";
+        std::cout << "Player 2 enter a spot to place an O (1-9) : ";
         std::cin >> number;
         number--;
         if (spaces[number] == ' ')
@@ -101,42 +101,42 @@ void player2Move(char* spaces, char player2)
     } while (!number > 0 || !number < 8);
     
 }
-bool checkWin(char* spaces, char player, char player2)
+bool checkWin(char* spaces, char player1, char player2)
 {
     //check row
     if ((spaces[0] != ' ') && (spaces[0] == spaces[1]) && (spaces[1] == spaces[2]))
     {
-        spaces[0] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+        spaces[0] == player1 ? std::cout << "Player1 WIN!\n" : std::cout << "Player2 WIN!\n";
     }
     else if ((spaces[3] != ' ') && (spaces[3] == spaces[4]) && (spaces[4] == spaces[5]))
     {
-        spaces[3] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+        spaces[3] == player1 ? std::cout << "Player1 WIN!\n" : std::cout << "Player2 WIN!\n";
     }
     else if ((spaces[6] != ' ') && (spaces[6] == spaces[7]) && (spaces[7] == spaces[8]))
     {
-        spaces[6] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+        spaces[6] == player1 ? std::cout << "Player1 WIN!\n" : std::cout << "Player2 WIN!\n";
     }
     //check column
     else if ((spaces[0] != ' ') && (spaces[0] == spaces[3]) && (spaces[3] == spaces[6]))
     {
-        spaces[0] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+        spaces[0] == player1 ? std::cout << "Player1 WIN!\n" : std::cout << "Player2 WIN!\n";
     }
     else if ((spaces[1] != ' ') && (spaces[1] == spaces[4]) && (spaces[4] == spaces[7]))
     {
-        spaces[1] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+        spaces[1] == player1 ? std::cout << "Player1 WIN!\n" : std::cout << "Player2 WIN!\n";
     }
     else if ((spaces[2] != ' ') && (spaces[2] == spaces[5]) && (spaces[5] == spaces[8]))
     {
-        spaces[2] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+        spaces[2] == player1 ? std::cout << "Player1 WIN!\n" : std::cout << "Player2 WIN!\n";
     }
     //check diag
     else if ((spaces[0] != ' ') && (spaces[0] == spaces[4]) && (spaces[4] == spaces[8]))
     {
-        spaces[0] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+        spaces[0] == player1 ? std::cout << "Player1 WIN!\n" : std::cout << "Player2 WIN!\n";
     }
     else if ((spaces[2] != ' ') && (spaces[2] == spaces[4]) && (spaces[4] == spaces[6]))
     {
-        spaces[2] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+        spaces[2] == player1 ? std::cout << "Player1 WIN!\n" : std::cout << "Player2 WIN!\n";
     }
     else
     {
