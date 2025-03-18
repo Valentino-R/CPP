7a9a6ed5605593a91eba1b94f3fdba4ccf8e85db
Valentino-R <valentino.orif@gmail.com>
2025-03-18 13:44:03 +0100

    fini

--- Exercices/ticTacToeV2.cpp
+++ Exercices/ticTacToeV2.cpp
@@ -4,7 +4,6 @@
 
 void drawBoard(char* spaces);
 void palyerMove(char* spaces, char player);
-void computerCounter(char* spaces, char player);
 void computerMove(char* spaces, char computer, char player);
 bool checkWin(char* spaces, char player, char computer);
 bool checkTie(char* spaces);
